20e42c8ad2fbe73bd29449f76a87a2429926cce4
John Smith <john.smith@gmail.com>
2025-01-19 19:32:24 -0500

    Initial commit: Simple Calculator Program in Go

---
+++ main.go
@@ -0,0 +1,39 @@
+//Mahmoud Sakalla
+//Simple Calculator Program, takes two numbers and performs the desired operation
+
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2 float64
+	var operator string
+
+	fmt.Println("Simple Calculator in Go")
+	fmt.Print("Enter the first number: ")
+	fmt.Scanln(&num1)
+
+	fmt.Print("Enter an operator (+, -, *, /): ")
+	fmt.Scanln(&operator)
+
+	fmt.Print("Enter the second number: ")
+	fmt.Scanln(&num2)
+
+	switch operator {
+	case "+":
+		fmt.Printf("Result: %.2f\n", num1+num2)
+	case "-":
+		fmt.Printf("Result: %.2f\n", num1-num2)
+	case "*":
+		fmt.Printf("Result: %.2f\n", num1*num2)
+	case "/":
+		if num2 != 0 {
+			fmt.Printf("Result: %.2f\n", num1/num2)
+		} else {
+			//make sure that a denominator cannot be zero
+			fmt.Println("Error: Division by zero.")
+		}
+	default:
+		fmt.Println("Invalid operator, choose from the options provided")
+	}
+}
